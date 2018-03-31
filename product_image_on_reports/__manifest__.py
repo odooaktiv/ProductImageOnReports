c1d8fb5faac015bdc8f5f4afc27dd523d1dd9706
odoo <alpesh@aktivsoftware.com>
2018-03-31 17:37:15 +0530

    [INIT] Initial commit for master branch.

---
+++ product_image_on_reports/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Product Image on Sales Invoice Purchase Report',
+    'version': '1.0',
+    'category': 'Sale',
+    'summary': '''
+        ''',
+    'author': 'Aktiv Software',
+    'depends': [
+        'sale', 'purchase'
+    ],
+    'website': 'www.aktivsoftware.com',
+    'data': [
+        'views/report_invoice.xml',
+        'views/sale_report_template.xml',
+        'views/purchase_order_templates.xml'
+
+    ],
+    'images': [],
+    'images': ['static/description/banner.jpg'],
+    'installable': True,
+    'installable': True,
+    'auto_install': False,
+}
