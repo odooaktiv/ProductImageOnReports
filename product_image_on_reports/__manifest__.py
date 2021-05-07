f9f2f9b43d4bd86e5334d3169a4add4b45239c9d
Aktiv Software <odoo@aktivsoftware.com>
2021-05-07 11:42:57 +0530

    [ADD]: Added module in 14.0

--- product_image_on_reports/__manifest__.py
+++ product_image_on_reports/__manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Product Image on Sales Invoice Purchase Report',
-    'version': '1.0',
-    'category': 'Sale',
-    'summary': '''
-        ''',
+    'name': 'Product Image on Reports',
+    'version': '14.0.1.0.0',
+    'category': 'Extra Tools',
+    'license': 'AGPL-3',
+    'summary': '''This module shows image of product on reports such as sales, invoice and purchase''',
     'author': 'Aktiv Software',
     'depends': [
-        'sale', 'purchase'
+        'sale_management', 'purchase'
     ],
     'website': 'www.aktivsoftware.com',
     'data': [
@@ -17,7 +17,6 @@
         'views/purchase_order_templates.xml'
 
     ],
-    'images': [],
     'images': ['static/description/banner.jpg'],
     'installable': True,
     'installable': True,
